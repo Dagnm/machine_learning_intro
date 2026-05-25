20e7172c590e91aaa6c02140510da04ed45d91f9
Dagnachew M. <dagnachew16@gmail.com>
2026-05-25 23:45:41 +0300

    may 25 night

--- Chapter_1/.ipynb_checkpoints/test-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from scipy import sparse
-from sklearn.datasets import load_iris
-x = np.array([[1, 2, 3], [4, 5, 6]])
-print("x = \n {}".format(x))
-
-eye = np.eye(4)
-print("4x4 Identity matrix = \n {}".format(eye))
